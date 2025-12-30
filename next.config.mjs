17205499a41f9860c163841544b80745c6b077e8
Lyra Attallah <lyrattallah@gmail.com>
2025-12-30 10:26:12 +0800

    deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,9 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig = {
-  ...(isProd && {
-    output: 'export',
-    trailingSlash: true,
-    basePath: '/Pocky',
-    assetPrefix: '/Pocky/',
-    images: {
-      unoptimized: true,
-    },
-  }),
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
--- out/_next/static/AfDNXSc08VZiQMsqLUcGi/_buildManifest.js
+++ out/_next/static/AfDNXSc08VZiQMsqLUcGi/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/Pocky//_next/:path+",
-        "destination": "/Pocky/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
